b5dcc1cc907276146bc9ff003d23073db954ce8a
tibazera <tiagozemuner@gmail.com>
2026-08-16 21:30:41 -0300

    CSQC: exclude independent spray hooks

--- src/pr2_cmds.c
+++ src/pr2_cmds.c
@@ -88,10 +88,6 @@ static intptr_t EXT_SetSendNeeded(intptr_t *args);
 #endif
 static intptr_t EXT_SetLastRuntime(intptr_t *args);
 static intptr_t EXT_MapExtFieldPtr(intptr_t *args);
-#ifdef MVD_PEXT1_SPRAYS
-static intptr_t EXT_SprayClear(intptr_t *args);
-static intptr_t EXT_SprayClearAll(intptr_t *args);
-#endif
 static intptr_t EXT_SetExtFieldPtr(intptr_t *args);
 static intptr_t EXT_GetExtFieldPtr(intptr_t *args);
 struct
@@ -107,10 +103,6 @@ struct
 	{"setsendneeded",		EXT_SetSendNeeded},
 #endif
 	{"SetLastRuntime",	EXT_SetLastRuntime},
-#ifdef MVD_PEXT1_SPRAYS
-	{"sprayclear",		EXT_SprayClear},
-	{"sprayclearall",	EXT_SprayClearAll},
-#endif
 };
 ext_syscall_t ext_syscall_tbl[256];
 
@@ -2205,19 +2197,6 @@ static intptr_t EXT_MapExtFieldPtr(intptr_t *args)
 
 	return 0;
 }
-
-#ifdef MVD_PEXT1_SPRAYS
-static intptr_t EXT_SprayClear(intptr_t *args)
-{
-	return SV_SpraysClearOne(args[1], true);
-}
-
-static intptr_t EXT_SprayClearAll(intptr_t *args)
-{
-	SV_SpraysClearAll(true);
-	return 1;
-}
-#endif
 
 /*
   int trap_Map_Extension( const char* ext_name, int mapto)
